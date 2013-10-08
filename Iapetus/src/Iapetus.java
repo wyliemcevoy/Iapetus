10d8e8cdcf92317b121cc0d7177626bd9d4effe7
Wylie <Wylie@10.0.0.7>
2013-10-08 09:46:32 -0400

    Robot robot.

--- Iapetus/src/Iapetus.java
+++ Iapetus/src/Iapetus.java
@@ -1,9 +1,23 @@
+import java.awt.AWTException;
+import java.awt.Robot;
+
 
 public class Iapetus {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world.");
+		try {
+			Robot robot = new Robot();
+			
+			
+			robot.mouseMove(300, 550);
+			
+			
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
